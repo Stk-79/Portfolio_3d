29dd612a626e5e7e0374785b835889e4bc544ecb
Sarthak Pati <sarthak07.dev@gmail.com>
2023-09-28 00:50:04 +0530

    transition delay for project cards.

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -13,7 +13,7 @@ const FeedbackCard = ({
   image,
 }) => (
   <motion.div
-    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    variants={fadeIn("", "spring", index * 0.5, 5.0)}
     className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
